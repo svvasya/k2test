6008cc9f8765dd2049508c9f6dc32997d3435474
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-31 10:14:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
 
+#setup(
+#    name='k2test',
+#    version='1.0.0',
+#    description='Description of my project',
+#    author='Your Name',
+#    author_email='yourname@example.com',
+#    packages=find_packages(),  
+#    package_data={'k2test': ['views.py']},
+#    install_requires=[
+
+ #   ],
+#)
 setup(
     name='k2test',
     version='1.0.0',
-    description='Description of my project',
-    author='Your Name',
-    author_email='yourname@example.com',
-    packages=find_packages(),  
-    package_data={'k2test': ['views.py']},
+    packages=['components.k2test'],
+    include_package_data=True,
     install_requires=[
-
+        'flask',
     ],
 )
