6ed4e9cae02ca0e3338965c2d41bca7f3ba06230
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-31 16:59:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,10 +15,16 @@ from setuptools import setup, find_packages
 setup(
     name='k2test',
     version='1.0.0',
+    description='Description of my project',
+    author='Your Name',
+    author_email='yourname@example.com',
+    keywords="k2test, k2",
+    python_requires=">=3.7, <4",
     packages=['k2test'],
-    include_package_data=True,
+    package_data={  # Optional
+        "k2test": ["main.py"],
+    },
     install_requires=[
-     
-        
+            
     ],
 )
