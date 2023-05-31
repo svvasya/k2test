f23073a5211493549710c7aff3613cdb9e558b87
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-31 17:05:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup, find_packages
 
-#setup(
-#    name='k2test',
-#    version='1.0.0',
-#    description='Description of my project',
-#    author='Your Name',
-#    author_email='yourname@example.com',
-#    packages=find_packages(),  
-#    package_data={'k2test': ['views.py']},
-#    install_requires=[
-  
- #   ],
-#)
 setup(
     name='k2test',
     version='1.0.0',
@@ -22,7 +10,7 @@ setup(
     python_requires=">=3.7, <=3.11.2",
     packages=['k2test'],
     package_data={  # Optional
-        "k2test": ["main.py"],
+        "k2test": ["main.py"],  "k2test": [static/style.css"], "k2test": [templates/"], 
     },
     install_requires=[
             
