553ab06a0700ee9b941cf15d236e785551483e10
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-31 17:01:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Your Name',
     author_email='yourname@example.com',
     keywords="k2test, k2",
-    python_requires=">=3.7, <=3.11",
+    python_requires=">=3.7, <=3.11.2",
     packages=['k2test'],
     package_data={  # Optional
         "k2test": ["main.py"],
