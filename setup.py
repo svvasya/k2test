e5943a6a929bed37d7c9444a60e13732646d13af
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-31 17:17:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires=">=3.7, <=3.11.2",
     packages=['k2test'],
     package_data={  # Optional
-        "k2test": ["static/style.css"]    },
+        "static": ["static/style.css"], "templates": ["templates/index.html", "templates/login.html"]    },
     install_requires=[
             
     ],
