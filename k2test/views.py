0e6dfced9d23d845a1ccb51d4b95f373d005444c
svidryk <102587970+svvasya@users.noreply.github.com>
2023-06-04 00:13:59 +0300

    Update views.py

--- k2test/views.py
+++ k2test/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request
 from flask_login import login_user, current_user,  logout_user, login_required
-from k2root.config import app
+from main import app
 from flask import Blueprint
 
 k2test_bp = Blueprint('k2test', __name__)
