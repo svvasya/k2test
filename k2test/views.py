f523a798a96c026d2860128e1bebd59fda98fd62
svidryk <102587970+svvasya@users.noreply.github.com>
2023-06-04 15:32:18 +0300

    Update views.py

--- k2test/views.py
+++ k2test/views.py
@@ -4,26 +4,25 @@ from flask import current_app as k2
 from flask import Blueprint, json
 from flask_sqlalchemy import SQLAlchemy
 
-app = k2
-db = k2.extensions['sqlalchemy'].db
 k2test_bp = Blueprint('k2test', __name__)
 
 @k2test_bp.route('/new')
 def new_component():    
-    with app.app_context():
-        connection = db.engine.connect()
-        query = db.text("SELECT email, login FROM k2users")
-        result = connection.execute(query)
-        rows = [dict(email=row.email, login=row.login) for row in result]
+    db = k2.extensions['sqlalchemy'].db
+    connection = db.engine.connect()
+    query = db.text("SELECT email, login FROM k2users")
+    result = connection.execute(query).fetchall()
 
+    rows = [dict(email=row.email, login=row.login) for row in result]
+    # Перетворення результату на JSON
+    json_result = json.dumps(rows)
 
-        # Перетворення результату на JSON
-        json_result = json.dumps(rows)
-
-        return json_result
-        k2users = result.fetchall()
+    return json_result
+    k2users = result.fetchall()
 
    
     return k2users
+    
+   
     
    
